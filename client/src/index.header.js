d9710ead5302d5e95088dbbfd873d1c092426abf
Jade "oatmealine" Monoids <oatmealine@disroot.org>
2026-05-18 21:48:01 +0300

    client: fix update URL

--- client/src/index.header.js
+++ client/src/index.header.js
@@ -7,8 +7,8 @@
 // @icon         https://raw.githubusercontent.com/oatmealine/yno-ap/refs/heads/main/common/ap-2kki.png
 // @homepage     https://github.com/oatmealine/yno-ap
 // @homepageURL  https://github.com/oatmealine/yno-ap
-// @downloadURL  https://github.com/oatmealine/yno-ap/releases/download/latest/yno-ap-client.user.js
-// @updateURL    https://github.com/oatmealine/yno-ap/releases/download/latest/yno-ap-client.user.js
+// @downloadURL  https://github.com/oatmealine/yno-ap/releases/latest/download/yno-ap-client.user.js
+// @updateURL    https://github.com/oatmealine/yno-ap/releases/latest/download/yno-ap-client.user.js
 // @match        https://ynoproject.net/2kki/
 // @grant        none
 // @run-at       document-end
